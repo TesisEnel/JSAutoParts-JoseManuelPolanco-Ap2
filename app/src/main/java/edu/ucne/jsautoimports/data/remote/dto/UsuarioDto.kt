e3e5e8a8bd8201fbd3d07dfdf3ebbf299bd880e1
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-12-04 17:34:20 -0400

    Implementar UsuarioDto

---
+++ app/src/main/java/edu/ucne/jsautoimports/data/remote/dto/UsuarioDto.kt
@@ -0,0 +1,10 @@
+package edu.ucne.jsautoimports.data.remote.dto
+
+data class UsuarioDto(
+    val usuarioId: Int? = null,
+    val nombre: String,
+    val email: String,
+    val password: String,
+)
+
+
