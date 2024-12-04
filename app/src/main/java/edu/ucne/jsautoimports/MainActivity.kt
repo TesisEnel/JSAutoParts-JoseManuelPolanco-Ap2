6ffe8783759fabefd62f2f28a5022792f4b39fe9
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-12-04 17:44:40 -0400

    Adaptar MainActivity al proyecto

--- app/src/main/java/edu/ucne/jsautoimports/MainActivity.kt
+++ app/src/main/java/edu/ucne/jsautoimports/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,20 +12,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.jsautoimports.presentation.login.AuthViewModel
+import edu.ucne.jsautoimports.presentation.navigation.AutoPartsNavHost
 import edu.ucne.jsautoimports.ui.theme.JSAutoImportsTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val authViewModel : AuthViewModel by viewModels()
+
         setContent {
             JSAutoImportsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navHost = rememberNavController()
+                AutoPartsNavHost(navHost, modifier = Modifier, authViewModel = authViewModel)
             }
         }
     }
