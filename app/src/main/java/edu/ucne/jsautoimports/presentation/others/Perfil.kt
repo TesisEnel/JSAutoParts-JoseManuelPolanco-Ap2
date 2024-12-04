d58de7a7e11f32e660b6122f5e6508c27bbd61b5
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-12-04 17:40:53 -0400

    Pantalla de Prueba para Perfil

---
+++ app/src/main/java/edu/ucne/jsautoimports/presentation/others/Perfil.kt
@@ -0,0 +1,240 @@
+package edu.ucne.jsautoimports.presentation.others
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import edu.ucne.jsautoimports.presentation.navigation.Screen
+import androidx.navigation.NavController
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    var isPersonalExpanded by remember { mutableStateOf(false) }
+    var selectedTab by remember { mutableStateOf("Profile") }
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                selectedTab = selectedTab,
+                navController = navController
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFF4F4F4))
+                .padding(paddingValues)
+        ) {
+            // Cabecera personalizada
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color(0xFF1A73E8), Color(0xFF1E88E5))
+                        )
+                    )
+                    .padding(16.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    // Imagen de perfil recortada dentro del círculo
+                    Box(
+                        modifier = Modifier
+                            .size(72.dp)
+                            .background(Color.White, shape = CircleShape) // Fondo blanco del marco
+                            .padding(4.dp) // Espacio entre el marco y la imagen
+                    ) {
+                        AsyncImage(
+                            model = "https://randomuser.me/api/portraits/men/1.jpg", // URL de ejemplo
+                            contentDescription = "Foto de perfil",
+                            modifier = Modifier
+                                .fillMaxSize() // Asegura que ocupe todo el espacio del contenedor
+                                .clip(CircleShape) // Recorta la imagen en un círculo
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    // Nombre y subtítulo
+                    Column {
+                        Text(
+                            text = "Samuel Duran",
+                            color = Color.White,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "Usuario Premium",
+                            color = Color(0xFFBBDEFB),
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .clickable { isPersonalExpanded = !isPersonalExpanded },
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.cardElevation(6.dp)
+            ) {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ShoppingCart,
+                        contentDescription = "Icono Personal",
+                        tint = Color(0xFF1A73E8),
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Carrito de compras",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+            }
+
+            // Mostrar carrito si está expandido
+            if (isPersonalExpanded) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                        .padding(16.dp)
+                ) {
+                    item {
+                        Text(
+                            text = "Productos en tu carrito",
+                            style = MaterialTheme.typography.titleLarge,
+                            color = Color(0xFF1A73E8),
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(bottom = 8.dp)
+                        )
+                    }
+                    item {
+                        ShoppingCartItem(
+                            productName = "Producto 1",
+                            productPrice = "DOP 500",
+                            imageUrl = "https://jsautoimports.blob.core.windows.net/imagenes/producto1.png"
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ShoppingCartItem(
+    productName: String,
+    productPrice: String,
+    imageUrl: String
+) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Imagen del producto
+            AsyncImage(
+                model = imageUrl,
+                contentDescription = "Imagen del producto",
+                modifier = Modifier
+                    .size(64.dp)
+                    .background(Color.LightGray, shape = CircleShape)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            // Información del producto
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = productName,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Text(
+                    text = productPrice,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF1A73E8)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    selectedTab: String,
+    navController: NavController
+) {
+    NavigationBar(
+        containerColor = Color.White,
+        tonalElevation = 4.dp
+    ) {
+
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.List, contentDescription = "Categories") },
+            label = { Text("Categories") },
+            selected = selectedTab == "Categories",
+            onClick = { navController.navigate(Screen.CategoriaListScreen) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.ShoppingCart, contentDescription = "Carrito") },
+            label = { Text("Carrito") },
+            selected = selectedTab == "Carrito",
+            onClick = { navController.navigate(Screen.CarritoScreen) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Person, contentDescription = "Account") },
+            label = { Text("Account") },
+            selected = selectedTab == "Account",
+            onClick = { navController.navigate(Screen.ProfileScreen) }
+        )
+    }
+}
