549eb0b30688a435e87859c85afd028f5938ca94
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-12-04 17:50:46 -0400

    Implementar y  diseñar CarritoListScreen.kt

---
+++ app/src/main/java/edu/ucne/jsautoimports/presentation/carrito/CarritoListScreen.kt
@@ -0,0 +1,284 @@
+package edu.ucne.jsautoimports.presentation.carrito
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import edu.ucne.jsautoimports.data.local.entities.CarritoDetalleEntity
+import edu.ucne.jsautoimports.presentation.CarritoViewModel
+import edu.ucne.jsautoimports.presentation.navigation.Screen
+import edu.ucne.proyectofinalaplicada2.presentation.components.TopBarComponent
+import kotlinx.coroutines.launch
+
+@Composable
+fun CartScreen(
+    viewModel: CarritoViewModel = hiltViewModel(),
+    navController: NavController
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+    val selectedTab = "Carrito"
+
+    CartBodyScreen(
+        uiState = uiState,
+        navController = navController,
+        selectedTab = selectedTab,
+        onCartEvent = { event ->
+            coroutineScope.launch {
+                viewModel.onUiEvent(event)
+            }
+        }
+    )
+}
+
+@Composable
+private fun CartBodyScreen(
+    uiState: CarritoUiState,
+    navController: NavController,
+    selectedTab: String,
+    onCartEvent: (CarritoUiEvent) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopBarComponent(
+                title = "Carrito de Compras",
+                onClickMenu = {},
+                onClickNotifications = {},
+                notificationCount = 0
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(
+                selectedTab = selectedTab,
+                navController = navController
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            CartProductList(uiState)
+
+            CartSummary(
+                uiState = uiState,
+                onProceedToCheckout = {
+                    onCartEvent(CarritoUiEvent.SaveCarritos)
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun CartProductList(uiState: CarritoUiState) {
+    Column(modifier = Modifier.padding(10.dp)) {
+        if (uiState.carritoDetalle.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Carrito vacío",
+                    modifier = Modifier.size(64.dp),
+                    tint = Color.Gray
+                )
+                Text(
+                    text = "Tu carrito está vacío",
+                    style = MaterialTheme.typography.bodyLarge.copy(color = Color.Gray)
+                )
+            }
+        } else {
+            LazyColumn {
+                items(uiState.carritoDetalle) { item ->
+                    CartRow(
+                        item = item
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TotalRow(label: String, amount: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = label, style = MaterialTheme.typography.bodyLarge)
+        Text(text = amount, style = MaterialTheme.typography.bodyLarge)
+    }
+}
+
+@Composable
+fun CartRow(
+    item: CarritoDetalleEntity,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        elevation = CardDefaults.cardElevation(5.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = item.carritoId.toString(),
+                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "Cantidad: ${item.cantidad}",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Gray
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Precio: $${item.precio}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Subtotal: $${item.subTotal}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
+                modifier = Modifier.size(40.dp)
+            ) {
+                Text(
+                    text = "X",
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CartSummary(uiState: CarritoUiState, onProceedToCheckout: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            TotalRow("Subtotal:", "$${uiState.subTotal}")
+            TotalRow("ITBS:", "$${uiState.impuesto}")
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                color = Color.Black,
+                thickness = 1.dp
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Total:", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                Text("$${uiState.total}", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+            }
+            Button(
+                onClick = onProceedToCheckout,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1A73E8)),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("Proceder al Pago", fontWeight = FontWeight.Bold)
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    selectedTab: String,
+    navController: NavController
+) {
+    NavigationBar(
+        containerColor = Color.White,
+        tonalElevation = 4.dp
+    ) {
+
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.List, contentDescription = "Categories") },
+            label = { Text("Categories") },
+            selected = selectedTab == "Categories",
+            onClick = { navController.navigate(Screen.CategoriaListScreen) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.ShoppingCart, contentDescription = "Carrito") },
+            label = { Text("Carrito") },
+            selected = selectedTab == "Carrito",
+            onClick = { navController.navigate(Screen.CarritoScreen) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Person, contentDescription = "Account") },
+            label = { Text("Account") },
+            selected = selectedTab == "Account",
+            onClick = { navController.navigate(Screen.ProfileScreen) }
+        )
+    }
+}
